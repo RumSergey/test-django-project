6e16d4aa7e37d688641b0d1afd3b2306ed1179de
Admin <rum_sergey@mail.ru>
2018-09-27 22:18:41 +0300

    test text form

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django import forms
 from .models import Post
+
+test_data = "initial"
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    test_data = "Ульяна"
+    test_data = "first_pressed"
+
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        test_data = "check_post"
+        if form.is_valid():
+            test_data = "is_valid"
+            test_data = form.cleaned_data['your_name']
+
     return render(request, 'blog/post_list.html', {'posts': posts,'test_data': test_data})
