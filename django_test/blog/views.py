76d01b8fce86c9edc2996d1044bce971efe5ce61
Admin <rum_sergey@mail.ru>
2018-09-28 21:46:34 +0300

    - four float values from and return sum

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -3,20 +3,22 @@ from django.utils import timezone
 from django import forms
 from .models import Post
 
-test_data = "initial"
-
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    begin_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+    begin_phi = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+    end_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+    end_phi = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    test_data = "first_pressed"
+    ret_code = 0.0
 
     if request.method == 'POST':
         form = NameForm(request.POST)
-        test_data = "check_post"
         if form.is_valid():
-            test_data = "is_valid"
-            test_data = form.cleaned_data['your_name']
+            ret_code =  form.cleaned_data['begin_lambda'] +  form.cleaned_data['begin_phi'] +  form.cleaned_data['end_lambda'] + form.cleaned_data['end_phi']
+    else :
+        form = NameForm()
 
-    return render(request, 'blog/post_list.html', {'posts': posts,'test_data': test_data})
+    return render(request, 'blog/post_list.html', {'ret_code': ret_code,'form': form})
