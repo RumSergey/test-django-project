2941ffe16a2cc34f940fea521563aa1760de94f8
Admin <rum_sergey@mail.ru>
2018-09-29 21:57:13 +0300

    - add pyroute

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -2,22 +2,34 @@ from django.shortcuts import render
 from django.utils import timezone
 from django import forms
 from .models import Post
+from pyroutelib3 import Router
+router = Router("foot")
 
 class NameForm(forms.Form):
-    begin_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
     begin_phi = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
-    end_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+    begin_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
     end_phi = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+    end_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    ret_code = 0.0
+    ret_code = 'none'
 
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            ret_code =  form.cleaned_data['begin_lambda'] +  form.cleaned_data['begin_phi'] +  form.cleaned_data['end_lambda'] + form.cleaned_data['end_phi']
+            start = router.findNode(form.cleaned_data['begin_phi'], form.cleaned_data['begin_lambda'])
+            end = router.findNode(form.cleaned_data['end_phi'],  form.cleaned_data['end_lambda'])
+
+            status, route = router.doRoute(start, end)
+            if status == 'success':
+                routeLatLons = list(map(router.nodeLatLon, route)) # Get actual route coordinates
+                ret_code = 'success'
+            else :
+                ret_code = status
+
+            #ret_code =  form.cleaned_data['begin_lambda'] +  form.cleaned_data['begin_phi'] +  form.cleaned_data['end_lambda'] + form.cleaned_data['end_phi']
     else :
         form = NameForm()
 
