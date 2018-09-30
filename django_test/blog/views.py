11fed1d21971170e825f69f9848eddef65efd848
Admin <rum_sergey@mail.ru>
2018-09-30 09:05:38 +0300

    - view list points

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -5,32 +5,43 @@ from .models import Post
 from pyroutelib3 import Router
 router = Router("foot")
 
+
 class NameForm(forms.Form):
-    begin_phi = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
-    begin_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
-    end_phi = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
-    end_lambda = forms.FloatField(required='True',max_value=90.0, min_value=-90.0)
+    begin_phi = forms.FloatField(
+        initial=56.8874, required='True', max_value=90.0, min_value=-90.0,widget=forms.NumberInput(attrs={'id': 'begin_phi', 'step': "0.0001"}))
+    begin_lambda = forms.FloatField(
+        initial=35.8652, required='True', max_value=90.0, min_value=-90.0,widget=forms.NumberInput(attrs={'id': 'begin_lambda', 'step': "0.0001"}))
+    end_phi = forms.FloatField(
+        initial=56.8843, required='True', max_value=90.0, min_value=-90.0,widget=forms.NumberInput(attrs={'id': 'end_phi', 'step': "0.0001"}))
+    end_lambda = forms.FloatField(
+        initial=35.8819, required='True', max_value=90.0, min_value=-90.0,widget=forms.NumberInput(attrs={'id': 'end_lambda', 'step': "0.0001"}))
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
     ret_code = 'none'
+
+    routeLatLons = list()
 
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            start = router.findNode(form.cleaned_data['begin_phi'], form.cleaned_data['begin_lambda'])
-            end = router.findNode(form.cleaned_data['end_phi'],  form.cleaned_data['end_lambda'])
+            start = router.findNode(
+                form.cleaned_data['begin_phi'], form.cleaned_data['begin_lambda'])
+            end = router.findNode(
+                form.cleaned_data['end_phi'],  form.cleaned_data['end_lambda'])
 
             status, route = router.doRoute(start, end)
             if status == 'success':
-                routeLatLons = list(map(router.nodeLatLon, route)) # Get actual route coordinates
+                # Get actual route coordinates
+                routeLatLons = list(map(router.nodeLatLon, route))
                 ret_code = 'success'
-            else :
+            else:
                 ret_code = status
 
             #ret_code =  form.cleaned_data['begin_lambda'] +  form.cleaned_data['begin_phi'] +  form.cleaned_data['end_lambda'] + form.cleaned_data['end_phi']
-    else :
+    else:
         form = NameForm()
 
-    return render(request, 'blog/post_list.html', {'ret_code': ret_code,'form': form})
+    return render(request, 'blog/post_list.html', {'ret_code': ret_code, 'form': form, 'route': routeLatLons})
