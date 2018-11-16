ea9c5aadbafaf0ac95929c4bc932c5d3b9c4114d
Admin <rum_sergey@mail.ru>
2018-11-16 21:16:03 +0300

    - время на складе

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -15,16 +15,16 @@ def hours_to_time_str(time_in_hours):
     hours = trunc( total_time )
     time_str = ''
     if hours > 0:
-        time_str = time_str + "{0:d}".format(hours) + 'ч:'
+        time_str = time_str + "{0:d}".format(hours) + ' ч: '
 
     total_time = total_time - hours
     total_time = total_time * 60
     minutes = trunc( total_time )
     if hours > 0:
-        time_str = time_str + "{0:d}".format(minutes) + 'м:'
+        time_str = time_str + "{0:d}".format(minutes) + ' мин: '
     else :
         if minutes > 0:
-            time_str = time_str + "{0:d}".format(minutes) + 'мин:'
+            time_str = time_str + "{0:d}".format(minutes) + ' мин: '
 
     total_time = total_time - minutes
     total_time = total_time * 60
@@ -32,14 +32,55 @@ def hours_to_time_str(time_in_hours):
     seconds = trunc( total_time )
 
     if hours > 0 or minutes > 0:
-        time_str = time_str + "{0:d}".format(seconds) + 'сек'
+        time_str = time_str + "{0:d}".format(seconds) + ' сек'
     else :
         if seconds > 0:
-            time_str = time_str + "{0:d}".format(seconds) + 'сек'
+            time_str = time_str + "{0:d}".format(seconds) + ' сек'
         else:
-            time_str = time_str + "{0:0.2f}".format(total_time) + 'сек'
+            time_str = time_str + "{0:0.2f}".format(total_time) + ' сек'
 
     return time_str
+
+def time_to_start_str(total, form_h, form_m):
+    if form_h == 0 and form_m == 0:
+        return ''
+    else :
+
+        count_hours = trunc( total )
+        total_time = total - count_hours
+        total_time = total_time * 60
+        count_minutes = trunc( total_time )
+
+        ret_m = 0
+
+        if form_m >= count_minutes :
+            ret_m = form_m - count_minutes
+        else :
+            ret_m = 60 - (count_minutes - form_m)
+            count_hours = count_hours + 1
+
+        ret_h = form_h - count_hours
+
+        if ret_h < 0:
+            ret_h = 24 - (count_hours - form_h)
+
+        ret = ', время старта ' + "{0:d}".format(ret_h) + ' ч: ' + "{0:d}".format(ret_m) + ' мин'
+
+        return ret
+
+def add_delay(h1, m1, h2, m2):
+    m1 = m1 - m2
+    add_h = 0
+    if m1 < 0:
+        m1 = m1 + 60
+        add_h = 1
+
+    h1 = h1 - h2 - add_h
+
+    if h1 < 0:
+        h1 = h1 + 24
+
+    return (h1,m1)
 
 
 class NameForm(forms.Form):
@@ -65,8 +106,15 @@ class NameForm(forms.Form):
         initial=56.8843, required='True', max_value=90.0, min_value=-90.0,widget=forms.NumberInput(attrs={'id': 'end_phi', 'step': "0.0001", 'onchange' : "onInputChanged()"}))
     end_lambda = forms.FloatField(label="Точка встречи, долгота",
         initial=35.8819, required='True', max_value=90.0, min_value=-90.0,widget=forms.NumberInput(attrs={'id': 'end_lambda', 'step': "0.0001", 'onchange' : "onInputChanged()"}))
-    #arrival_time = forms.TimeField(label="Требуемое время прибытия",required='False', widget=forms.TimeInput(format='%H:%M'))
     wheel_val = forms.ChoiceField(choices=techlist,label="Вид транспорта", initial='car', required=True, widget=forms.Select(attrs={'id': 'wheel_val'}))
+    arrival_time_hour = forms.IntegerField(label="Время прибытия, ч",
+        required='False', initial=0, min_value=0, max_value=24, widget=forms.NumberInput(attrs={'id': 'arr_time_hour', 'step': "1", 'onchange' : "onInputChanged()"}))
+    arrival_time_minutes = forms.IntegerField(label="Время прибытия, м",
+        required='False', initial=0, min_value=0, max_value=60, widget=forms.NumberInput(attrs={'id': 'arr_time_minutes', 'step': "1", 'onchange' : "onInputChanged()"}))
+    sclad_time_hour = forms.IntegerField(label="Ожидание на складе, ч",
+        required='False', initial=0, min_value=0, max_value=24, widget=forms.NumberInput(attrs={'id': 'sclad_time_hour', 'step': "1", 'onchange' : "onInputChanged()"}))
+    sclad_time_minutes = forms.IntegerField(label="Ожидание на складе, м",
+        required='False', initial=0, min_value=0, max_value=60, widget=forms.NumberInput(attrs={'id': 'sclad_time_minutes', 'step': "1", 'onchange' : "onInputChanged()"}))
 
 
 
@@ -94,6 +142,18 @@ def post_list(request):
             bound_min_phi = form.cleaned_data['begin_phi']
             bound_max_phi = form.cleaned_data['end_phi']
             transport = form.cleaned_data['wheel_val']
+            ar_time_h = form.cleaned_data['arrival_time_hour']
+            ar_time_m = form.cleaned_data['arrival_time_minutes']
+            scl_time_h = form.cleaned_data['sclad_time_hour']
+            scl_time_m = form.cleaned_data['sclad_time_minutes']
+
+            print('before')
+            print(ar_time_h,ar_time_m)
+            print('scl_time_h',scl_time_h)
+            ar_time_h,ar_time_m = add_delay(ar_time_h,ar_time_m,scl_time_h,scl_time_m)
+            print('after')
+            print(ar_time_h,ar_time_m)
+
 
             router = Router(transport)
 
@@ -121,6 +181,7 @@ def post_list(request):
             status, route = router.doRoute(start, med)
 
             sum_length = 0
+            time_str_1 = ''
             if status == 'success':
                 # Get actual route coordinates
                 routeLatLons = list(map(router.nodeLatLon, route))
@@ -154,9 +215,9 @@ def post_list(request):
 
                 #ret_code = 'success'
                 #ret_code = '1-й маршрут построен, ' + 'время в пути ' + "{0:.2f}".format(sum_length / speed_list[transport]) + ' ч, '
-                ret_code = 'маршрут до склада построен, время в пути '
-                time_str = hours_to_time_str(sum_length / speed_list[transport])
-                ret_code = ret_code + time_str
+                #ret_code = 'время в пути до склада '
+                time_str_1 = hours_to_time_str(sum_length / speed_list[transport])
+                #ret_code = ret_code + time_str
             else:
                 if ret_code == 'none':
                     ret_code = 'маршрут до склада отсутствует, '
@@ -165,6 +226,7 @@ def post_list(request):
 
             status, route = router.doRoute(med, end)
             sum_length2 = 0
+            time_str_2 = ''
 
             if status == 'success':
                 # Get actual route coordinates
@@ -199,16 +261,20 @@ def post_list(request):
                         bound_min_la = point[1]
 
                 #ret_code = 'success'
-                ret_code = ret_code + ', маршрут до точки встречи построен, время в пути '
-                time_str = hours_to_time_str(sum_length2 / speed_list[transport])
-                ret_code = ret_code + time_str
+                #ret_code = ret_code + '; время в пути до точки встречи '
+                time_str_2 = hours_to_time_str(sum_length2 / speed_list[transport])
+                #ret_code = ret_code + time_str
             else:
                 if ret_code == 'none':
                     ret_code = 'маршрут до точки встречи отсутствует, '
                 else:
                     ret_code = 'маршрут до точки встречи не построен, '
 
-            ret_code = ret_code + ', общая длина маршрута ' +  "{0:.2f}".format(sum_length + sum_length2)  + ' км, время расчёта ' + "{0:.2f}".format(time.time() - start_time) + ' сек '
+            ret_code = 'общая длина маршрута ' +  "{0:.2f}".format(sum_length + sum_length2)  + ' км'
+            ret_code = ret_code + ', время движения ' +  hours_to_time_str((sum_length + sum_length2) / speed_list[transport])
+            #ret_code = ret_code + ' ( ' + time_str_1 + ' до склада, ' + time_str_2 + ' до точки встречи)'
+            ret_code = ret_code + time_to_start_str( (sum_length + sum_length2) / speed_list[transport], ar_time_h,ar_time_m)
+            ret_code = ret_code + '; время выполнения расчётов ' + "{0:.2f}".format(time.time() - start_time) + ' сек '
 
 
     elif request.method == 'GET':
